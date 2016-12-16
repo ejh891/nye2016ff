2e911067d87b04437873d821b7a553a84a82aa03
EJ Hammond <ejh891@gmail.com>
2016-12-16 14:26:14 -0500

    Add basic auth on manage page

--- server.js
+++ server.js
@@ -49,8 +49,38 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
+var basicAuthPass = process.env.BASICAUTHPASS || "";
 app.get('/manage', function(req, res) {
-    res.sendFile(__dirname + '/public/views/manage.html');
+    var auth = req.headers['authorization'];  // auth is in base64(username:password)  so we need to decode the base64
+    console.log("Authorization Header is: ", auth);
+
+    if(!auth) { // No Authorization header was passed in so it's the first time the browser hit us
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        res.end('<html><body>Need some creds son</body></html>');
+    }
+    else if(auth) {    // The Authorization was passed in so now we validate it
+        var tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+
+        var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
+        var plain_auth = buf.toString();        // read it back out as a string
+
+        console.log("Decoded Authorization ", plain_auth);
+
+        // At this point plain_auth = "username:password"
+        var creds = plain_auth.split(':');      // split on a ':'
+        var username = creds[0];
+        var password = creds[1];
+
+        if(password === basicAuthPass) {   // Is the username/password correct?
+            res.sendFile(__dirname + '/public/views/manage.html');
+        }
+        else {
+            res.statusCode = 401; // Force them to retry authentication
+            res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+            res.end('<html><body>You shall not pass</body></html>');
+        }
+    }
 });
 
 app.get('/litCheck', function(req, res) {
